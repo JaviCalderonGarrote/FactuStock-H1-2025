91baaa89e5520c721c012935c9f1e6fc40e18119
JaviCalderon <javier.calderon@evidem.com>
2025-02-11 10:58:09 +0100

    Estructura de React

---
+++ Front/factustock/src/App.jsx
@@ -0,0 +1,12 @@
+import ProductosTable from "./components/ProductosTable";
+
+function App() {
+  return (
+    <div>
+      <h1>FactuStock - Productos</h1>
+      <ProductosTable />
+    </div>
+  );
+}
+
+export default App;
